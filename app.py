245d7ba8081f591e0268ddfa463e4bae0b90a62e
karshievnavruz <qarshiyevnavruz7777@gmail.com git config --global user.email qarshiyevnavruz7777@gmail.com>
2023-02-16 17:22:36 +0500

    init

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request
+from flask import Flask ,request,jsonify
 import requests
-import os
+import telegram
 from telegram import Bot, Update
+from telegram.ext import Dispatcher,CommandHandler,MessageHandler,Filters
+import os
+
 from main import(
-    start
+    start,
+    echo,
 )
 # flask app
 app = Flask(__name__)
@@ -12,18 +16,29 @@ app = Flask(__name__)
 TOKEN = os.environ['TOKEN']
 bot = Bot(TOKEN)
 
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.method=='GET':
-        return 'hi'
-    elif request.method=='POST':
-        data=request.get_json(force=True)
-        
-        dispatcher:Dispatcher=Dispatcher(bot,None,worker=0)
-        update:Update=Update.de_json(data,bot)
-        
+    if request.method == 'GET':
+        return 'Helo World'
+    elif request.method == 'POST':
+
+        data = request.get_json(force=True)
+
+        dispatcher:Dispatcher = Dispatcher(bot, None , workers=0)
+
+        update:Update = Update.de_json(data, bot)
+
         dispatcher.add_handler(CommandHandler('start',callback=start))
-        dispatcher.add_handler(MessageHandler('start',callback=start))
+        dispatcher.add_handler(MessageHandler(Filters.text,echo))
+
         dispatcher.process_update(update)
-        
-        return 'hi'
+
+        # chat_id = update.message.chat_id
+        # text = update.message.text
+        # if text !=None:
+
+        #     bot.send_message(chat_id, text)
+        # print(chat_id)
+
+    return "Assalomu alaykum"
--- main.py
+++ main.py
@@ -5,4 +5,6 @@ from telegram.ext import CallbackContext
 def start(update:Update,context:CallbackContext):
     update.message.reply_text('Hello')
   
-    
+def echo(update:Update, context:CallbackContext):
+    text = update.message.text
+    update.message.reply_text(text)
